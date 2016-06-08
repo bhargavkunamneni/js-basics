70900135e4466a612585ae6304df2495357804cc
bhargavkunamneni <bhargav.kunamneni@gmail.com>
2016-06-08 19:12:17 +0530

    added some other code

--- loop.js
+++ loop.js
@@ -15,3 +15,16 @@ function getCarsList(data){
 console.log(getCarsList("ciaz"));
 console.log(getCarsList("c2000"));
 console.log(getCarsList("wagonar"));
+
+var employees=[{name:"surendra",salary:52000},{name:"balaram",salary:20000},{name:"ashok",salary:18000}];
+
+var i=0;
+function employeeDetails(data){
+while(i<data.length){
+  console.log(i);
+  console.log(data[i].name,data[i].salary);
+  i++;
+}
+  }
+employeeDetails(employees);
+
