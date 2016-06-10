a01226fe72707415e794a05441ddeb1c7ad79864
bhargavkunamneni <bhargav.kunamneni@gmail.com>
2016-06-10 20:43:42 +0530

    added three files in to git

---
+++ form.js
@@ -0,0 +1,9 @@
+function changeDetails(){
+  var firstName=document.getElementById("firstName");
+  var lastNmae =document.getElementById("lastName");
+  firstName.value="surendra";
+  lastName.value="yarraguntla";
+  var age=document.getElementById("age");
+  age.value=23;
+  
+}
